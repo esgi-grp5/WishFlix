043dc7e06a30d7fd25fdc490a40cd785f4f6c141
Yanow0 <yan.parmentier@gmail.com>
2022-07-21 23:48:39 +0200

    add: post/delete to wishlist + insert/delete in db + buttons

--- lib/models/movie_model.dart
+++ lib/models/movie_model.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'dart:math';
+
+import 'package:wishflix/Screens/main.dart' as rootPage;
 
 import 'package:flutter/material.dart';
 import 'package:wishflix/Screens/ViewScreens/UniversalViewScreen.dart';
@@ -7,7 +10,10 @@ import 'package:wishflix/Widgets/DisplayItem/RateSection.dart';
 import 'package:wishflix/Widgets/DisplayItem/RowInformationsWithTitle.dart';
 import 'package:wishflix/Widgets/DisplayItem/RowTimesWishlisted.dart';
 import 'package:wishflix/Widgets/DisplayItem/RowWishListInfo.dart';
+import 'package:wishflix/Widgets/General/OAuth.dart';
 import 'package:wishflix/models/base_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:wishflix/repository/movie_repository.dart';
 
 class Movie extends Base {
   Movie(
@@ -16,7 +22,12 @@ class Movie extends Base {
       required String genre,
       required String dateSortie,
       required String name})
-      : super(id:id, image: image, genre: genre, dateSortie: dateSortie, name: name);
+      : super(
+            id: id,
+            image: image,
+            genre: genre,
+            dateSortie: dateSortie,
+            name: name);
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
@@ -38,6 +49,22 @@ class Movie extends Base {
 
   @override
   void goToPage(context) {
+    final OAuth oAuth = OAuth();
+    String token = oAuth.getToken();
+
+    /*var response = await http.get(
+      Uri.http('87.106.171.75:3000', '/movie/wishlist', {
+        "id": "1",
+      }),
+      headers: <String, String>{
+        'Authorization': 'Bearer $token',
+        HttpHeaders.contentTypeHeader: 'application/json'
+      },
+    );
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+    debugPrint('--------- Hello response code : ${response.statusCode}'); */
+
     List<Widget> screenBody = [];
 
     screenBody.add(ImageTitle(
@@ -45,6 +72,64 @@ class Movie extends Base {
         subTitle1: "2h23",
         subTitle2: this.genre,
         imageAssetUrl: this.image));
+    screenBody.add(
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: rootPage.appTheme.backgroundColor,
+          minimumSize: const Size.fromHeight(50), // NEW
+        ),
+        onPressed: () async {
+          MovieRepository _movieRepository = MovieRepository();
+          _movieRepository.insertMovie(this);
+          final http.Response response = await http.post(
+              Uri.parse('http://87.106.171.75:3000/movie/wishlist'),
+              headers: <String, String>{
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer $token',
+              },
+              body: jsonEncode(
+                <String, int>{"username_id": 1, "movie_id": this.id},
+              ));
+          print('Token: $token');
+          print('Request: ${response.request}');
+          print('Response status: ${response.statusCode}');
+          print('Response body: ${response.body}');
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
+    );
+    screenBody.add(
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: rootPage.appTheme.primaryColor,
+          minimumSize: const Size.fromHeight(50), // NEW
+        ),
+        onPressed: () async {
+          MovieRepository _movieRepository = MovieRepository();
+          _movieRepository.deleteMovie(this.id);
+          final http.Response response = await http.delete(
+              Uri.parse('http://87.106.171.75:3000/movie/wishlist'),
+              headers: <String, String>{
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer $token',
+              },
+              body: jsonEncode(
+                <String, int>{"username_id": 1, "movie_id": this.id},
+              ));
+          print('Token: $token');
+          print('Request: ${response.request}');
+          print('Response status: ${response.statusCode}');
+          print('Response body: ${response.body}');
+        },
+        child: const Icon(
+          Icons.remove,
+          color: Colors.white,
+        ),
+      ),
+    );
     screenBody
         .add(RowWishListInfo(dateSortie: this.dateSortie, estVuText: "Non vu"));
     screenBody.add(RowInformationsWithTitle(
